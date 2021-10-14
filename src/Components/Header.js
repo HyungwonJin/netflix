af0113258279e616a953c9ef196307fd184aae6b
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-10-14 14:17:47 +0900

    chore: use styled-components

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const SHeader = styled.header``;
+
+const List = styled.ul`
+    display: flex;
+    &:hover{
+        background-color: blue;
+    }
+`;
+
+const SLink = styled(Link)``;
+
+const Item = styled.li``;
 
 const Header = () => (
-    <header>
-        <ul>
-            <li>
-                <a href="/">Moives</a>
-            </li>
-            <li>
-                <a href="/tv">TV</a>
-            </li>
-            <li>
-                <a href="/search">Search</a>
-            </li>
-        </ul>
-    </header>
+    <SHeader>
+        <List>
+            <Item>
+                <SLink to="/">Moives</SLink>
+            </Item>
+            <Item>
+                <SLink to="/tv">TV</SLink>
+            </Item>
+            <Item>
+                <SLink to="/search">Search</SLink>
+            </Item>
+        </List>
+    </SHeader>
 )
 
 export default Header;
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,9 +3,11 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import Home from 'Routes/Home'
 import TV from 'Routes/TV'
 import Search from 'Routes/Search'
+import Header from './Header'
 
 const Router = () => (
     <BrowserRouter>
+        <Header />
         <Switch>
             <Route path='/' exact component={Home} />
             <Route path='/tv' component={TV} />
